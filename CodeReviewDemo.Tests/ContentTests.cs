cd49b7239fede3a3b1a92f771bde9d00e872ed0d
AQUEDUCT\rose <rose@aqueduct.co.uk>
2016-10-05 15:11:49 +0100

    Initial Commit

---
+++ CodeReviewDemo.Tests/ContentTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeReviewDemo.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeReviewDemo.Tests
+{
+    [TestClass]
+    public class ContentTests
+    {
+        private const string Hello = "Goodbye 1";
+
+        [TestMethod]
+        public void CotentGet_Success()
+        {
+            Assert.AreEqual(Hello, new Content().Get());
+
+        }
+    }
+}
---
+++ CodeReviewDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeReviewDemo.MvcApplication" Language="C#" %>
